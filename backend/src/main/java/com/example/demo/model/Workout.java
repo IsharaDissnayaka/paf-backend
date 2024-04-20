fb81b66d22b20866935134817e0df91fa5d7d8a8
IT21174780 <IT21174780@my.sliit.lk>
2024-04-21 02:47:04 +0530

    new file:   backend/.gitignore 	new file:   backend/.mvn/wrapper/maven-wrapper.jar 	new file:   backend/.mvn/wrapper/maven-wrapper.properties 	new file:   backend/mvnw 	new file:   backend/mvnw.cmd 	new file:   backend/pom.xml 	new file:   backend/src/main/java/com/example/demo/Controller/WorkoutController.java 	new file:   backend/src/main/java/com/example/demo/DemoApplication.java 	new file:   backend/src/main/java/com/example/demo/model/Workout.java 	new file:   backend/src/main/java/com/example/demo/repository/WorkoutRepository.java 	new file:   backend/src/main/java/com/example/demo/service/WorkoutService.java 	new file:   backend/src/main/resources/application.properties 	new file:   backend/src/test/java/com/example/demo/DemoApplicationTests.java

---
+++ backend/src/main/java/com/example/demo/model/Workout.java
@@ -0,0 +1,77 @@
+package com.example.demo.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "workouts")
+public class Workout {
+    
+    @Id
+    private String id;
+
+    private String workoutState;
+    private String description;
+    private Date date;
+    private List<WorkoutStateDetail> state;
+
+    // Constructor
+    public Workout() {
+        this.state = new ArrayList<>();
+    }
+
+    public Workout(String workoutState,Date date,String description) {
+        this.workoutState = workoutState;
+        this.date=date;
+        this.description=description;
+        this.state = new ArrayList<>();
+    }
+
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getWorkoutState() {
+        return workoutState;
+    }
+
+    public void setWorkoutState(String workoutState) {
+        this.workoutState = workoutState;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<WorkoutStateDetail> getState() {
+        return state;
+    }
+
+    public void setState(List<WorkoutStateDetail> state) {
+        this.state = state;
+    }
+
+    // Inner class for Workout state detail
+    public static record WorkoutStateDetail(String name, float completed, float burend_callary) {}
+}
